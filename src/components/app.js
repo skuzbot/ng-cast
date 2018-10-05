6499be7d9e79607225c9507bef8a88b41e9cc2e1
Steven Duncan <steven.k.duncan@gmail.com>
2018-10-05 17:27:49 -0500

    make vL and vLE work on live-server, not yet on spec

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ angular.module('video-player')
   .component('app', {
     templateUrl: './src/templates/app.html',
     controller: function() {
-
+      this.videos = exampleVideoData;
     }
   }
   );
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,10 +1,8 @@
 angular.module('video-player')
   .component('videoList', {
     templateUrl: './src/templates/videoList.html',
-    controller: function() {
-      let $ctrl = this;
-      $ctrl.videoList = exampleVideoData;
-      //console.log('vL data', $ctrl.videoList);
-    }
-    
-  });
+    controller: function () {
+      this.videos = exampleVideoData;
+    },
+    //bindings: { videos: '<' },
+  }); 
